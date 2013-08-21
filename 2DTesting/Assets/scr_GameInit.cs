8d7de66167ac1bb15ad0dd179b5dea80b588ee2d
Joshua McGee <josh@dsmedialabs.com>
2013-08-21 09:30:51 -0400

    Adds the initial 2D sprite project.

---
+++ 2DTesting/Assets/scr_GameInit.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scr_GameInit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Screen.SetResolution(240, 160, false);
+	}
+}
